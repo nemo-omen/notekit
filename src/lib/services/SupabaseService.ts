49bfc6f8356e4670ce08635324a152a337a949b1
nemo-omen <nemo.omen@gmail.com>
2022-01-03 20:00:19 -0600

    Check load from supabase

--- src/lib/services/SupabaseService.ts
+++ src/lib/services/SupabaseService.ts
@@ -3,9 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const url = import.meta.env.VITE_SUPABASE_URL;
 const key = import.meta.env.VITE_SUPABASE_KEY;
 
-const supabase = createClient(url, key);
-
-// 1. Make requests
-// 2. Return either stored data or error object --> Result Class?
-
-export class SupabaseService {}
+export const supabase = createClient(url, key);
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,28 @@
+<script context="module">
+  import { supabase } from '../lib/services/SupabaseService.ts';
+
+  export async function load({ fetch, url }) {
+    const { data, error } = await supabase.from('notes').select('*');
+    if (!error) {
+      console.log(data);
+      return {
+        props: {
+          notes: data
+        }
+      };
+    }
+    console.error(error);
+    return {
+      props: {
+        error: error
+      }
+    };
+  }
+</script>
+
 <script>
+  export let notes;
+  export let error;
 </script>
 
 <h1>Testing</h1>
