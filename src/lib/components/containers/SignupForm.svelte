71c2f77e1ce0bd0db79bf16e63e98b957ed800d8
nemo-omen <nemo.omen@gmail.com>
2022-01-04 08:17:01 -0600

    SignupForm validation more or less works

--- src/lib/components/containers/SignupForm.svelte
+++ src/lib/components/containers/SignupForm.svelte
@@ -1,38 +1,55 @@
-<script>
+<script lang="ts">
   import { goto } from '$app/navigation';
   import { browser } from '$app/env';
 
-  let email = '';
-  let confirmation = '';
-  let isConfirmed = true;
-  let isValid = false;
+  let form: HTMLFormElement;
+  let email: string = '';
+  let password: string = '';
+  let confirmation: string = '';
+  let isValidPasswordLength: boolean = true;
+  let isValidEmail: boolean = true;
+  let isConfirmed: boolean = true;
+  let isValidFormState: boolean = false;
 
   async function signup() {
-    if (confirm() === false) {
-      const response = await fetch('/signup', {
-        method: 'post',
-        body: new FormData(e.target)
-      });
+    const response = await fetch('/signup', {
+      method: 'post',
+      body: new FormData(form)
+    });
 
-      if (browser) {
-        if (response.ok) goto('/notebook');
-      }
+    if (browser) {
+      if (response.ok) goto('/notebook');
     }
   }
 
-  function confirm() {
-    isConfirmed = !(email === confirmation);
+  function confirm(e) {
+    isConfirmed = password === e.target.value;
+    validateFormState();
     return isConfirmed;
   }
 
-  function validateEmail() {
-    isValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-    return isValid;
+  function validatePasswordLength(e) {
+    isValidPasswordLength = e.target.value.trim().length > 6;
+    validateFormState();
+    return isValidPasswordLength;
+  }
+
+  function validateEmail(e) {
+    isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+      e.target.value
+    );
+    validateFormState();
+    return isValidEmail;
+  }
+
+  function validateFormState() {
+    isValidFormState = isValidPasswordLength && isValidEmail && isConfirmed;
+    return isValidFormState;
   }
 </script>
 
 <h2>Sign Up</h2>
-<form on:submit|preventDefault={signup}>
+<form on:submit|preventDefault={signup} bind:this={form}>
   <div class="input-group">
     <label for="email">Email Address</label>
     <input
@@ -41,11 +58,20 @@
       id="email"
       placeholder="you@example.com"
       bind:value={email}
+      on:blur={validateEmail}
+      class={isValidEmail ? '' : 'invalid'}
     />
   </div>
   <div class="input-group">
     <label for="password">Password</label>
-    <input type="password" name="password" id="password" />
+    <input
+      type="password"
+      name="password"
+      id="password"
+      bind:value={password}
+      on:blur={validatePasswordLength}
+      class={isValidPasswordLength ? '' : 'invalid'}
+    />
   </div>
   <div class="input-group">
     <label for="email">Password Confirmation</label>
@@ -54,15 +80,15 @@
       name="confirmation"
       id="confirmation"
       bind:value={confirmation}
-      class={isConfirmed ? '' : 'unconfirmed'}
+      class={isConfirmed ? '' : 'invalid'}
       on:blur={confirm}
     />
   </div>
-  <input type="submit" value="Sign Up" disabled={!isConfirmed ? true : false} />
+  <input type="submit" value="Sign Up" disabled={!isValidFormState} />
 </form>
 
 <style>
-  .unconfirmed {
+  .invalid {
     border: 1px solid tomato;
     background: #ffd9d2;
   }
