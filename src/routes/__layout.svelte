312dfd49256a6a7cddb2d37027604171822f5c8e
nemo-omen <nemo.omen@gmail.com>
2022-01-06 18:28:25 -0600

    Get rid of $session muckery in __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,6 @@
-<script context="module">
-  export async function load({ url, session }) {
-    return {
-      props: {
-        email: session
-      }
-    };
-  }
-</script>
-
 <script>
   import Header from '$components/containers/Header.svelte';
-  import { session } from '$app/stores';
-  import { browser } from '$app/env';
   import '../app.scss';
-  export let email;
-
-  if (browser) {
-    if (email) $session = email;
-  }
 </script>
 
 <Header />
