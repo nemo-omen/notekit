1ee4f9a11d8852bb9d018b44d98a8dd036e3d352
nemo-omen <nemo.omen@gmail.com>
2022-01-08 20:20:42 -0600

    Basic front page layout

--- src/lib/components/containers/Header.svelte
+++ src/lib/components/containers/Header.svelte
@@ -1,14 +1,22 @@
 <script>
   import { session } from '$app/stores';
+  import Logo from '$components/UI/Logo.svelte';
+  import GithubLogo from '$components/UI/GithubLogo.svelte';
+  import LinkButton from '$components/UI/LinkButton.svelte';
 </script>
 
 <header>
+  <a href="/" class="brand">
+    <Logo />
+    <span class="brand-text">NoteKit</span>
+  </a>
   <nav>
     {#if !$session}
-      <a href="/">Home</a>
-      <a href="/signin">Sign Up/Log In</a>
+      <LinkButton innerText="Sign Up (it's free)" href="/signin" />
+      <a href="https://github.com/nemo-omen/notekit" class="github-link">
+        <GithubLogo />
+      </a>
     {:else}
-      <a href="/">Home</a>
       <a href="/notebook">Notebook</a>
       <a href="/profile">Profile</a>
     {/if}
@@ -16,8 +24,48 @@
 </header>
 
 <style>
+  header {
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    margin-block: 1rem;
+    font-family: var(--sans);
+    font-weight: 600;
+    font-size: 1.25rem;
+  }
   nav {
     display: flex;
+    align-items: center;
     gap: 2rem;
+  }
+
+  .brand {
+    font-size: 2rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .brand-text {
+    font-size: 1.5rem;
+  }
+
+  .github-link {
+    font-size: 2rem;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    color: #333333;
+  }
+
+  .github-link:hover {
+    color: #222222;
+  }
+
+  @media (min-width: 1280px) {
+    header {
+      width: 70%;
+      align-items: center;
+    }
   }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,6 +27,8 @@
 </script>
 
 <script>
+  import NotebookPens from '$components/NotebookPens.svelte';
+  import LinkButton from '$components/UI/LinkButton.svelte';
   export let notes;
   export let error;
   export let user;
@@ -34,10 +36,62 @@
   console.log({ notes });
 </script>
 
-<h1>Your Notebooks</h1>
+<div class="hero">
+  <div class="message">
+    <p class="heading-message">
+      <span class="bold-text">NoteKit</span> makes notetaking simple.
+    </p>
+    <p class="heading-message">
+      It syncs where you need it to, links easily between documents, and lets
+      you edit with whoever you want.
+    </p>
+    <LinkButton innerText="Sign Up (it's free)" href="/signin" />
+  </div>
+  <div class="hero-image">
+    <NotebookPens />
+  </div>
+</div>
 
 <style lang="scss">
-  h1 {
-    color: $tomato;
+  .hero {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 8rem;
+    margin-block: 8rem;
+    font-family: var(--serif);
+  }
+  .hero-image {
+    display: flex;
+    align-items: center;
+    flex-basis: 1;
+  }
+
+  .message {
+    display: flex;
+    flex-direction: column;
+    // justify-content: space-between;
+    gap: 3rem;
+    align-items: flex-start;
+    flex-basis: 1;
+  }
+  .bold-text {
+    font-weight: 600;
+  }
+  h2 {
+    font-weight: 800;
+    letter-spacing: 0.05em;
+    color: tomato;
+    font-size: 2rem;
+  }
+  .heading-message {
+    font-size: 1.75rem;
+    letter-spacing: 0.125em;
+    font-weight: 500;
+  }
+  .heading-callout {
+    font-size: 2.5rem;
+    color: tomato;
+    font-weight: 800;
+    letter-spacing: 0.075em;
   }
 </style>
